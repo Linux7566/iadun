663ed0b96e2a65e258af8b1e8b9ef4ba40b76065
Linux7566 <alina0090263@gmail.com>
2025-06-21 10:30:43 +0300

    Create day00_ex05

---
+++ day00_ex05.sql
@@ -0,0 +1,3 @@
+SELECT (SELECT name FROM person WHERE person.id = person_order.person_id) 
+FROM person_order
+WHERE person_order.menu_id IN (13, 14, 18) AND person_order.order_date = ('2022-01-07')
