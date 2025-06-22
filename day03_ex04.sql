bbaae141bc4d1f69c6699d9bc9d66ed8220dd26f
Linux7566 <alina0090263@gmail.com>
2025-06-22 15:53:26 +0300

    Create day03_ex04.sql

--- day03_ex04.sql
+++ day03_ex04.sql
@@ -1,9 +1,9 @@
 WITH pop AS (
   SELECT p.name AS pizzeria_name, per.gender
-  FROM person_order po
-  JOIN person per ON po.person_id = per.id
-  JOIN menu m ON po.menu_id = m.id
-  JOIN pizzeria p ON m.pizzeria_id = p.id)
+  FROM person_order
+  JOIN person ON po.person_id = per.id
+  JOIN menu ON po.menu_id = m.id
+  JOIN pizzeria ON m.pizzeria_id = p.id)
 SELECT DISTINCT pizzeria_name
 FROM (
   SELECT pizzeria_name FROM pop WHERE gender = 'male'
