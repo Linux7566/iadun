2384e0b112df7d941069f5229c6451ec02f40ef7
Linux7566 <alina0090263@gmail.com>
2025-06-22 15:54:29 +0300

    Update day03_ex05

--- day03_ex05.sql
+++ day03_ex05.sql
@@ -1,7 +1,7 @@
 SELECT pizzeria.name AS pizzeria_name
 FROM person_visits
 JOIN person ON pv.person_id = person.id
-JOIN pizzeria pz ON pv.pizzeria_id = pizzeria.id
+JOIN pizzeria ON pv.pizzeria_id = pizzeria.id
 LEFT JOIN person_order ON po.person_id = person.id
   JOIN menu ON po.menu_id = menu.id AND m.pizzeria_id = pizzeria.id
 WHERE person.name = 'Andrey' AND po.menu_id IS NULL
